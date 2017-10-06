124716fb1b7a57cefafa41a4f3b27caf2358d8be
Frank Moreno <frankmoreno1993@gmail.com>
2017-10-06 07:32:54 -0500

    example for dataframe encodin

--- dataframe/example/example_dataframe.go
+++ dataframe/example/example_dataframe.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "fmt"
   sdt "github.com/kranfix/siser/dataframe"
 )
 
@@ -8,7 +9,7 @@ func main(){
   dht := sdt.Dht{T:23.5,H:51.7}
   fmt.Println(dht)
 
-  gps := sdt.Gps{La:-22.01,H:-17.04}
+  gps := sdt.Gps{La:-22.01,Lo:-17.04}
   fmt.Println(gps)
 
   sDt := sdt.Dataframe{750, 16, dht, gps, 50}
