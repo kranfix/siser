a824e7d6940a9dc2a8c26c90858df5ce8e38a11c
Frank Moreno <frankmoreno1993@gmail.com>
2017-10-11 17:55:16 -0500

    siserx: last version for final test

--- siserx/siserx.go
+++ siserx/siserx.go
@@ -18,7 +18,7 @@ func main(){
   c := &serial.Config{
     Name: "/dev/ttyUSB0",
     Baud: 9600,
-    ReadTimeout: 100 * time.Millisecond,
+    ReadTimeout: 3000 * time.Millisecond,
   }
   s, err := serial.OpenPort(c)
   if err != nil {
@@ -40,7 +40,7 @@ func main(){
 	//mqtt.ERROR = log.New(os.Stdout, "", 0)
 	opts := mqtt.NewClientOptions().AddBroker("tcp://190.90.6.43:1883").SetClientID("")
 	opts.SetKeepAlive(2 * time.Second)
-	opts.SetPingTimeout(1000 * time.Millisecond)
+	opts.SetPingTimeout(3000 * time.Millisecond)
 
   mqttClient := mqtt.NewClient(opts)
 	if token := mqttClient.Connect(); token.Wait() && token.Error() != nil {
@@ -52,33 +52,34 @@ func main(){
   Bytes := make([]byte, 128)
   var N int
   xbeeBuf := []byte("REQ_")
-  xbeeMax := byte(6)
+  xbeeMax := byte(7)
   RequestXbee:
   for i := byte(1); i <= xbeeMax; i++ {
     // Request of data
-    xbeeBuf[3] = 7
+    //RequestData:
+    println("Requesting to device: ",i)
+    xbeeBuf[3] = i
     s.Write(xbeeBuf)
 
-    // Waiting for response
-    for {
-      // Reading serialport
-      if N, err = s.Read(Bytes); err != nil {
-        continue
-      }
+    // Reading serialport
+    if N, err = s.Read(Bytes); err != nil {
+      println("Device ",i," unavailable")
+      continue
+    }
 
-      readed := false
-      for n,k:= 0,0; n < N; n += k {
-        k,readed = sDt.Detect(Bytes[n:N])
-        if readed {
-          //fmt.Printf("% x\n",Dt.Bytes())
-          fmt.Println(sDt)
-          token := mqttClient.Publish("incuba/peru", 0, false, sDt.String())
-          token.Wait()
-          goto RequestXbee
-        }
+    readed := false
+    for n,k:= 0,0; n < N; n += k {
+      k,readed = sDt.Detect(Bytes[n:N])
+      if readed {
+        fmt.Println(sDt)
+        token := mqttClient.Publish("incuba/peru", 0, false, sDt.String())
+        token.Wait()
       }
     }
   }
+  println("waiting 8 seconds\n\n\n\n\n")
+  time.Sleep(8 * time.Second)
   goto RequestXbee
+
   fmt.Println("Unexpected infite loop exit.")
 }
