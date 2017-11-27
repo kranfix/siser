b5da8d51c1588c3101889e35d33e3e83679234de
Frank Moreno <frankmoreno1993@gmail.com>
2017-11-27 18:22:17 -0500

    siserZip: luxes

---
+++ arduino/siserZip/lx.cpp
@@ -0,0 +1,22 @@
+#include "lx.h"
+#include <Wire.h>
+
+byte buff[2];
+
+void BH1750_Init(int address) {
+  Wire.beginTransmission(address);
+  Wire.write(0x10); // 1 [lux] aufloesung
+  Wire.endTransmission();
+}
+
+byte BH1750_Read(int address) {
+  byte i = 0;
+  Wire.beginTransmission(address);
+  Wire.requestFrom(address, 2);
+  while (Wire.available()) {
+    buff[i] = Wire.read();
+    i++;
+  }
+  Wire.endTransmission();
+  return i;
+}
---
+++ arduino/siserZip/lx.h
@@ -0,0 +1,2 @@
+void BH1750_Init(int address);
+byte BH1750_Read(int address);
