2cf7772acb32b5191e1529a4b8796b18629d651b
Frank Moreno <frankmoreno1993@gmail.com>
2017-12-05 12:03:36 -0500

    siser.zip: config

---
+++ arduino/siserZip/config.h
@@ -0,0 +1,35 @@
+/* Sensors pinout configurations */
+
+// Debug
+//#define DEBUG
+
+// Device id:
+//    type: byte or uint8_t
+//    from 0 to 255
+// 0 id is reserved for raspberry
+// 7 is reserved for demo in office
+// Use from 1 to 6
+#define ID 5
+
+
+// xbeeSerial
+#define xbeeSerial Serial
+#define XBEE_INIT {'R','E','Q'}
+#define XBEE_INIT_LEN 3
+
+// MQ2 sensor
+#define mq2Pin A0
+
+// Rain sensor
+#define rainPin A1
+
+
+// DHT sensor
+#define DHTPIN 7
+#define DHTTYPE DHT22
+
+// GPS
+#define gpsSerial Serial2
+
+// Lx sensor
+#define BH1750_address 0x23
