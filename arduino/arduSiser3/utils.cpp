870cecf4360149a8e107fd719247b75a9297d90b
Frank Moreno <frankmoreno1993@gmail.com>
2018-06-12 11:04:29 -0500

    utils for the project

---
+++ arduino/arduSiser3/utils.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include "utils.h"
+
+int averageAnalogRead(int pinToRead, uint8_t N){
+  unsigned int sum = 0; 
+  for(uint8_t i = 0 ; i < N ; i++){
+    sum += analogRead(pinToRead);
+  }
+  return sum / N;  
+}
+
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max){
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
---
+++ arduino/arduSiser3/utils.h
@@ -0,0 +1,6 @@
+// Created by Rossy Uscamaita on June 8th, 2018
+#include <stdint.h>
+
+int averageAnalogRead(int pinToRead, uint8_t N);
+
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max);
